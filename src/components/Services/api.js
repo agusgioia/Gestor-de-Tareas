6c600663bc6b8d14a812e3663e8dede6dac2e8af
agusgioia <agusgioia@gmail.com>
2025-05-11 18:04:24 -0300

    ajustes

--- src/components/Services/api.js
+++ src/components/Services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "https://gestordeproyectosytareas.netlify.app/api/boards";
+const API_BASE = "https://ripe-things-arrive.loca.lt/api/boards";
 
 export const getBoards = async () => {
   try {
