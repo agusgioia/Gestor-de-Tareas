63bd36400ea05d2d40291fc92ad1e05ddc249415
agusgioia <agusgioia@gmail.com>
2025-06-03 14:38:16 -0300

    variables de entorno

--- src/components/Services/firebase.js
+++ src/components/Services/firebase.js
@@ -4,15 +4,15 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth'; // Import onAuthSta
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC6EQ502yOnZqUb-D84V7I6NPHTcMO2wbA",
-    authDomain: "aw-gioia-agustin.firebaseapp.com",
-    databaseURL: "https://aw-gioia-agustin-default-rtdb.firebaseio.com",
-    projectId: "aw-gioia-agustin",
-    storageBucket: "aw-gioia-agustin.firebasestorage.app",
-    messagingSenderId: "406720255064",
-    appId: "1:406720255064:web:20dedaeaa199d06d46662d",
-    measurementId: "G-K4KMTKQEYZ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 
